6b440a2dda257280c92a470ab4cace5bd0d7a806
PowerF18 <sanboh04@gmail.com>
2024-02-05 17:56:33 +0100

    Se agregó fecha y hora en formato inglés

--- dart_application_1/bin/dart_application_1.dart
+++ dart_application_1/bin/dart_application_1.dart
@@ -2,4 +2,10 @@ import 'package:dart_application_1/dart_application_1.dart' as dart_application_
 
 void main(List<String> arguments) {
   print('Hello world: ${dart_application_1.calculate()}!');
+  // Obtener la fecha y hora actual
+    DateTime now = DateTime.now();
+    // Formatear la fecha y hora
+    String formattedDateTime = "${now.year}-${now.month}-${now.day} ${now.hour}:${now.minute}:${now.second}";
+    // Imprimir el mensaje con la fecha y hora
+    print('Hello world: ${dart_application_1.calculate()}! ($formattedDateTime)');
 }
